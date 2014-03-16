99306560702b6b10f46f38e27b19f70562f03c2e
Chris Keathley <spyc3r@gmail.com>
2014-03-16 13:47:27 -0400

    Initial commit

---
+++ lib/message/gun.rb
@@ -0,0 +1,7 @@
+require "message/gun/version"
+
+module Message
+  module Gun
+    # Your code goes here...
+  end
+end
---
+++ lib/message/gun/version.rb
@@ -0,0 +1,5 @@
+module Message
+  module Gun
+    VERSION = "0.0.1"
+  end
+end
